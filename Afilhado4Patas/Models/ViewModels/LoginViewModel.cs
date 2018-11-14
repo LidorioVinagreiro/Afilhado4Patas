8224472d79cc7497f36fc844b8fadb953fb2ac41
LidorioVinagreiro <33322949+LidorioVinagreiro@users.noreply.github.com>
2018-11-14 22:13:15 +0000

    Login view model básico

--- Afilhado4Patas/Models/ViewModels/LoginViewModel.cs
+++ Afilhado4Patas/Models/ViewModels/LoginViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +9,11 @@ namespace Afilhado4Patas.Models.ViewModels
 {
     public class LoginViewModel
     {
+        [DisplayName("Email")]
+        [DataType(DataType.EmailAddress)]
+        public String Email { get; set; }
+        [DisplayName("Password")]
+        [DataType(DataType.Password,ErrorMessage ="Password Inválida")]
+        public String Password { get; set; }
     }
 }
