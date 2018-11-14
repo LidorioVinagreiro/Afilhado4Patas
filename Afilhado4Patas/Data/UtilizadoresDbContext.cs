ed1971600d6ca32edf77a2b0d919eb9b11cbd62a
LidorioVinagreiro <33322949+LidorioVinagreiro@users.noreply.github.com>
2018-11-14 20:45:55 +0000

    v1 dev

---
+++ Afilhado4Patas/Data/UtilizadoresDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Afilhado4Patas.Models.Utilizadores;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Afilhado4Patas.Data
+{
+    public class UtilizadoresDbContext : IdentityDbContext<Utilizadores>
+    {
+        
+    }
+}
--- Afilhado4Patas/Models/Utilizadores/Utilizadores.cs
+++ Afilhado4Patas/Models/Utilizadores/Utilizadores.cs
@@ -1,11 +1,13 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Afilhado4Patas.Models.Utilizadores
 {
-    public class Utilizadores
+    public class Utilizadores : IdentityUser
     {
+        public Perfil Perfil { get; set; }
     }
 }
