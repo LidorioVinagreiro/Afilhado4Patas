4c3d0b3d6847b4cf0c5e691d4c646c265f5c8ba2
Lainfor <rmlainfor@gmail.com>
2018-11-17 21:17:02 +0000

    Tarefa 3, 5 e 6 - Controllers Home, Funcionario e Responsavel do Canil

--- Afilhado4Patas/Controllers/TiposUtilizadores/ResponsavelController.cs
+++ Afilhado4Patas/Controllers/TiposUtilizadores/ResponsavelController.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Afilhado4Patas.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Afilhado4Patas.Controllers.TiposUtilizadores
@@ -11,6 +13,41 @@ namespace Afilhado4Patas.Controllers.TiposUtilizadores
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Adotar()
+        {
+            return View();
+        }
+
+        public IActionResult Doar()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
