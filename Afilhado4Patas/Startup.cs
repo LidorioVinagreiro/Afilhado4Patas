2196f8412aa7eba14d85f04343f1ad386059bb42
Daniel Dias <daniel_._19@hotmail.com>
2018-11-19 22:17:12 +0000

    commit db connection string

--- Afilhado4Patas/Startup.cs
+++ Afilhado4Patas/Startup.cs
@@ -34,19 +34,27 @@ namespace Afilhado4Patas
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
-            
+
+            services.AddDefaultIdentity<Utilizadores>()
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+
+
             // Use SQL Database if in Azure, otherwise, use SQLite
-            if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Production")
+            /*if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Production")
                 services.AddDbContext<ApplicationDbContext>(options =>
                         options.UseSqlServer(Configuration.GetConnectionString("MyDbConnection")));
             else
                 services.AddDbContext<ApplicationDbContext>(options =>
                         options.UseSqlServer("Data Source=localdatabase.db"));
             // Automatically perform database migration
-            services.BuildServiceProvider().GetService<ApplicationDbContext>().Database.Migrate();
-            services.AddDefaultIdentity<Utilizadores>()
-                .AddEntityFrameworkStores<ApplicationDbContext>();
+            services.BuildServiceProvider().GetService<ApplicationDbContext>().Database.Migrate();*/
 
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            // Automatically perform database migration
+            services.BuildServiceProvider().GetService<ApplicationDbContext>().Database.Migrate();
+
+            
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
