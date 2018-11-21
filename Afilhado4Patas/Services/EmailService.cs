1786edfa76856f988d8693c12ee30d4f1ae76e08
Daniel Dias <daniel_._19@hotmail.com>
2018-11-21 18:14:54 +0000

    add package sendgrid

---
+++ Afilhado4Patas/Services/EmailService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Afilhado4Patas.Services
+{
+    public class EmailService
+{
+        public string SendGridUser { get; set; }
+        public string SendGridKey { get; set; }
+}
+}
