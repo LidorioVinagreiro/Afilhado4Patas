898738c938be9dd7cbf83bdbcd622080c824c521
Daniel Dias <daniel_._19@hotmail.com>
2018-11-23 14:58:55 +0000

    email envio

--- Afilhado4Patas/Areas/Identity/Services/EmailSender.cs
+++ Afilhado4Patas/Areas/Identity/Services/EmailSender.cs
@@ -1,9 +1,9 @@
 ﻿using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.Azure.KeyVault;
-using Microsoft.Azure.Services.AppAuthentication;
 using Microsoft.Extensions.Options;
-using SendGrid;
-using SendGrid.Helpers.Mail;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using System;
+using System.Diagnostics;
 using System.Net;
 using System.Net.Mail;
 using System.Threading.Tasks;
@@ -12,18 +12,21 @@ namespace Afilhado4Patas.Areas.Identity.Services
 {
     public class EmailSender : IEmailSender
     {
+        public EmailSender(IOptions<AuthMessageSenderOptions> optionsAccessor)
+        {
+            Options = optionsAccessor.Value;
+        }
+
+        public AuthMessageSenderOptions Options { get; } //set only via Secret Manager
+
         public Task SendEmailAsync(string email, string subject, string message)
         {
             return Execute(subject, message, email);
         }
-
         public Task Execute(string subject, string message, string email)
         {
-            var azureServiceTokenProvider = new AzureServiceTokenProvider();
-            var keyVaultClient = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(azureServiceTokenProvider.KeyVaultTokenCallback));
-            var secret_user = keyVaultClient.GetSecretAsync("https://SendGridAutentication.vault.azure.net/secrets/SenGridUser").ConfigureAwait(false);
-            var secret_key = keyVaultClient.GetSecretAsync("https://SendGridAutentication.vault.azure.net/secrets/SenGridKey").ConfigureAwait(false);
-
+            string user = Options.SendGridUser;
+            string key = Options.SendGridKey;
             var mailMessage = new MailMessage
             {
                 From = new MailAddress("afilhados4patas@gmail.com", "Afilhados4Patas"),
@@ -34,15 +37,14 @@ namespace Afilhado4Patas.Areas.Identity.Services
             mailMessage.IsBodyHtml = true;
             mailMessage.To.Add(email);
 
-
             var smtpClient = new SmtpClient
             {
-                Credentials = new NetworkCredential(secret_user.ToString(), secret_key.ToString()),
+                Credentials = new NetworkCredential(user, key),
                 Host = "smtp.sendgrid.net",
                 Port = 587
             };
 
             return smtpClient.SendMailAsync(mailMessage);
-        }
+        }        
     }
 }
