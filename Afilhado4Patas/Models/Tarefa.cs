be26110c0e5c746e0d9c6bc10df2146ebeceaa5e
Lainfor <rmlainfor@gmail.com>
2018-12-07 08:13:11 +0000

    Modelo Tarefa

---
+++ Afilhado4Patas/Models/Tarefa.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Afilhado4Patas.Models
+{
+    public class Tarefa
+    {
+        [Key]
+        public int Id { get; set; }
+        [Display(Name = "Funcionario")]
+        public string FuncionarioId { get; set; }
+        [Display(Name = "Data de Inicio")]
+        public DateTime Inicio { get; set; }
+        [Display(Name = "Data de Fim")]
+        public DateTime Fim { get; set; }
+        [Display(Name = "Descricao")]
+        public string Descricao { get; set; }
+        public bool Completada { get; set; }
+    }
+}
