745b6a64f9e3ca9eab5a432ff0376edeb54b3ba4
Daniel Dias <daniel_._19@hotmail.com>
2018-12-08 23:06:00 +0000

    update branch

--- Afilhado4Patas/Controllers/TiposUtilizadores/UtilizadorController.cs
+++ Afilhado4Patas/Controllers/TiposUtilizadores/UtilizadorController.cs
@@ -3,13 +3,23 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Afilhado4Patas.Data;
 using Afilhado4Patas.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Afilhado4Patas.Controllers.TiposUtilizadores
 {
     public class UtilizadorController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public UtilizadorController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -29,11 +39,6 @@ namespace Afilhado4Patas.Controllers.TiposUtilizadores
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         public IActionResult Adotar()
         {
             return View();
@@ -42,6 +47,103 @@ namespace Afilhado4Patas.Controllers.TiposUtilizadores
         public IActionResult Doar()
         {
             return View();
+        }
+
+
+        // GET: Perfil/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user = _context.Utilizadores.Where(u => u.Email == id).Include(p => p.Perfil).FirstOrDefault();
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return View(user);
+        }
+
+        // GET: Perfil/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Perfil/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Perfil/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var editarPerfilViewModel = _context.Utilizadores.Where(e => e.Email == id).ToList().FirstOrDefault();
+            Perfil perfil = editarPerfilViewModel.Perfil;
+            if (editarPerfilViewModel == null)
+            {
+                return NotFound();
+            }
+            return View(perfil);
+        }
+
+        // POST: EditarPerfil/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("Id,FirstName,LastName,Street,City,Postalcode,NIF,Photo,Birthday")] Perfil editarPerfilViewModel)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(editarPerfilViewModel);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PerfilExists(editarPerfilViewModel.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(editarPerfilViewModel);
+        }
+
+        private bool PerfilExists(int id)
+        {
+            return _context.PerfilTable.Any(e => e.Id == id);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
