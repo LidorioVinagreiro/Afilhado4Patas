6c85ab963453f4e05622b3193f1eb7c8b5a97a29
Daniel Dias <daniel_._19@hotmail.com>
2018-12-16 21:08:14 +0000

    hotfix testes unitarios

---
+++ Afilhado4PatasTestesUnitarios/ControllersTestes/FuncionarioControllerTest.cs
@@ -0,0 +1,93 @@
+using Afilhado4Patas.Controllers.TiposUtilizadores;
+using Afilhado4Patas.Models;
+using System;
+using Xunit;
+using Microsoft.Data.Sqlite;
+using Microsoft.CodeAnalysis;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Afilhado4Patas.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Afilhado4PatasTest.ControllersTest
+{
+    public class FuncionarioControllerTest
+    {
+        public string connectionString = "DataSource=:memory:";
+        public SqliteConnection connection;
+        public DbContextOptions<ApplicationDbContext> options;
+        
+        public FuncionarioControllerTest()
+        {
+            connection = new SqliteConnection(connectionString);
+            connection.Open();
+            options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseSqlite(connection)
+                .Options;
+        }
+
+        [Fact]
+        public async Task Index_CanLoadFromContext()
+        {
+            using (var context = new ApplicationDbContext(options))
+            {
+                var controller = new FuncionarioController(context);
+                var result = controller.Index();
+                var viewResult = Assert.IsType<ViewResult>(result);
+                Assert.IsType<ViewResult>(result);
+            }
+        }
+
+        [Fact]
+        public async Task About_CanLoadFromContext()
+        {
+            using (var context = new ApplicationDbContext(options))
+            {
+                var controller = new FuncionarioController(context);
+                var result = controller.About();
+                var viewResult = Assert.IsType<ViewResult>(result);
+                Assert.IsType<ViewResult>(result);
+            }
+        }
+
+        [Fact]
+        public async Task Contact_CanLoadFromContext()
+        {
+            using (var context = new ApplicationDbContext(options))
+            {
+                var controller = new FuncionarioController(context);
+                var result = controller.Contact();
+                var viewResult = Assert.IsType<ViewResult>(result);
+                Assert.IsType<ViewResult>(result);
+            }
+        }
+
+        [Fact]
+        public async Task Adotar_CanLoadFromContext()
+        {
+            using (var context = new ApplicationDbContext(options))
+            {
+                var controller = new FuncionarioController(context);
+                var result = controller.Adotar();
+                var viewResult = Assert.IsType<ViewResult>(result);
+                Assert.IsType<ViewResult>(result);
+            }
+        }
+
+        [Fact]
+        public async Task Doar_CanLoadFromContext()
+        {
+            using (var context = new ApplicationDbContext(options))
+            {
+                var controller = new FuncionarioController(context);
+                var result = controller.Doar();
+                var viewResult = Assert.IsType<ViewResult>(result);
+                Assert.IsType<ViewResult>(result);
+            }
+        }
+    }
+}
---
+++ Afilhado4PatasTestesUnitarios/ControllersTestes/UtilizadorControllerTest.cs
@@ -0,0 +1,132 @@
+﻿using Afilhado4Patas.Controllers.TiposUtilizadores;
+using Afilhado4Patas.Data;
+using Afilhado4Patas.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Moq;
+
+namespace Afilhado4PatasTest.ControllersTest
+{
+    public class UtilizadorControllerTest
+    {
+        public string connectionString = "DataSource=:memory:";
+        public SqliteConnection connection;
+        public DbContextOptions<ApplicationDbContext> options;
+        public ApplicationDbContext contextDB;
+
+        public UtilizadorControllerTest()
+        {
+            connection = new SqliteConnection(connectionString);
+            connection.Open();
+            options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseSqlite(connection)
+                .Options;
+            contextDB = new ApplicationDbContext(options);
+            contextDB.Database.EnsureCreated();
+            contextDB.Roles.AddRange(
+                    new IdentityRole("Responsavel"),
+                    new IdentityRole("Funcionario"),
+                    new IdentityRole("Utilizador"));
+                Utilizadores utilizador1 = new Utilizadores
+                {
+                    UserName = "danieldias@gmail.com",
+                    Email = "danieldias@gmail.com",
+                    EmailConfirmed = true,
+                    Active = true
+                };
+                string password = "daniel123";
+                GetMockUserManager().Object.CreateAsync(utilizador1, password);
+                Perfil perfilResponsavel = new Perfil
+                {
+                    UtilizadorId = utilizador1.Id,
+                    FirstName = "Daniel",
+                    LastName = "Dias",
+                    Genre = "Masculino",
+                    Birthday = new DateTime(1990, 10, 10),
+                    Street = "Rua Das Flores",
+                    NIF = "123456789",
+                    Postalcode = "2564-568",
+                    City = "Setubal"
+                };
+            contextDB.PerfilTable.Add(perfilResponsavel);
+            utilizador1.PerfilId = perfilResponsavel.Id;
+                GetMockUserManager().Object.AddToRoleAsync(utilizador1, "Utilizador");                
+                contextDB.SaveChanges();
+        }
+
+        [Fact]
+        public async Task Index_CanLoadFromContext()
+        {
+            using (contextDB)
+            {
+                var controller = new UtilizadorController(contextDB);
+                var result = controller.Index();
+                var viewResult = Assert.IsType<ViewResult>(result);
+                Assert.IsType<ViewResult>(result);
+            }
+        }
+
+        [Fact]
+        public async Task About_CanLoadFromContext()
+        {
+            using (contextDB)
+            {
+                var controller = new UtilizadorController(contextDB);
+                var result = controller.About();
+                var viewResult = Assert.IsType<ViewResult>(result);
+                Assert.IsType<ViewResult>(result);
+            }
+        }
+
+        [Fact]
+        public async Task Contact_CanLoadFromcontextDB()
+        {
+            using (contextDB)
+            {
+                var controller = new UtilizadorController(contextDB);
+                var result = controller.Contact();
+                var viewResult = Assert.IsType<ViewResult>(result);
+                Assert.IsType<ViewResult>(result);
+            }
+        }
+
+        [Fact]
+        public async Task Adotar_CanLoadFromcontextDB()
+        {
+            using (contextDB)
+            {
+                var controller = new UtilizadorController(contextDB);
+                var result = controller.Adotar();
+                var viewResult = Assert.IsType<ViewResult>(result);
+                Assert.IsType<ViewResult>(result);
+            }
+        }
+
+        [Fact]
+        public async Task Doar_CanLoadFromcontextDB()
+        {
+            using (contextDB)
+            {
+                var controller = new UtilizadorController(contextDB);
+                var result = controller.Doar();
+                var viewResult = Assert.IsType<ViewResult>(result);
+                Assert.IsType<ViewResult>(result);
+            }
+        }
+
+        private Mock<UserManager<Utilizadores>> GetMockUserManager()
+        {
+            var userStoreMock = new Mock<IUserStore<Utilizadores>>();
+            return new Mock<UserManager<Utilizadores>>(
+                userStoreMock.Object, null, null, null, null, null, null, null, null);
+        }
+    }
+}
