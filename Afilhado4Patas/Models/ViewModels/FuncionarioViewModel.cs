bc65bef54d6119f90330568c8a594ce7bf20628d
Daniel Dias <daniel_._19@hotmail.com>
2018-12-19 21:34:39 +0000

    bugfix

--- Afilhado4Patas/Models/ViewModels/FuncionarioViewModel.cs
+++ Afilhado4Patas/Models/ViewModels/FuncionarioViewModel.cs
@@ -36,10 +36,10 @@ namespace Afilhado4Patas.Models.ViewModels
         public string Genero { get; set; }
 
         [Required(ErrorMessage = "Preencha este campo com a sua Password!")]
+        [PasswordAtLeast1LetterAnd1Number]
         [RegularExpression(@"^[a-zA-Z0-9]+$", ErrorMessage = "A sua password deverá ter letras (aA) e pelo menos 1 numero e não deverá ter caracteres que não letras ou numeros")]
         [StringLength(15, ErrorMessage = "A {0} deverá ter pelo menos {2} e um maximo de {1} caracteres de comprimento.", MinimumLength = 8)]
         [DataType(DataType.Password)]
-        [PasswordAtLeast1LetterAnd1Number]
         [Display(Name = "Palavra-Passe")]
         public string Password { get; set; }
 
